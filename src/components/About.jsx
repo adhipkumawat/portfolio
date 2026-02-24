e6fcc841c03e1cf405964986e67f86d8e23bedce
Adhip <adhipkumawat@gmail.com>
2026-02-24 16:51:43 +0530

    hy

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ function About  () {
   return (
     <div className=' w-180 ml-120 mt-10'>
         <h1 className='font-bold ml-5 text-2xl'>About</h1>
-        <p className='ml-5 text-1xl text-gray-500 mt-3 capitalize'> end of 2020 I completed my school, then i took a drop of 1 year and joined a course in web devlopment. after completing that,<a className='underline text-black'> i took a bca cource with specalization in ai/ml (artifical inteligency with machine learning)</a> in 2023 from jecrc univercity </p>
+        <p className='ml-5 text-1xl text-gray-500 mt-3 capitalize'>at the end of 2020 I completed my school, then i took a drop of 1 year and joined a course in web devlopment. after completing that,<a className='underline text-black'> i took a bca cource with specalization in ai/ml (artifical inteligency with machine learning)</a> in 2023 from jecrc univercity </p>
 
     </div>
   )
